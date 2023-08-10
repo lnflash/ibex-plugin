7543f0063dcf86e1b9e19bf62608094b5110ff81
Purke Gopinand <38141108+PurkeGopinand@users.noreply.github.com>
2023-08-10 20:57:22 +0530

    Update lnurlpay.ts

--- src/ibex/app/router/lnurlpay.ts
+++ src/ibex/app/router/lnurlpay.ts
@@ -7,6 +7,7 @@ const router: Router = express.Router();
 router.post('/', ibexAuthMiddleWare, createLNURLHandler);
 router.get('/invoiceRequirement', ibexAuthMiddleWare,  invoiceRequirementHandler);
 router.get('/pay',  payInvoiceHandler);
+router.get('/decodeLNURL',  decodeLNURLHandler);
 
 
 export default router;
