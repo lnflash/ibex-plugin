35686a98461c7136ef32f370ab699e7d16154611
Dread <34528298+islandbitcoin@users.noreply.github.com>
2023-08-14 10:58:33 -0600

    Cleanup/flash (#4)
    * cleanup
    
    * Update README.md

--- src/ibex/app/router/index.ts
+++ src/ibex/app/router/index.ts
@@ -1,25 +1,22 @@
-import express, { Router } from 'express';
-import accountRoutes from '@ibex/app/router/account';
-import lightningAddressRouter from '@ibex/app/router/lightningaddress';
-import lightningInvoiceRouter from '@ibex/app/router/lightninginvoice';
-import lightningPaymentRouter from '@ibex/app/router/lightningpayment';
-import currencyRouter from '@ibex/app/router/currency';
-import onchainRouter from '@ibex/app/router/onchain';
+import express, { Router } from "express";
+import accountRoutes from "@ibex/app/router/account";
+import lightningAddressRouter from "@ibex/app/router/lightningaddress";
+import lightningInvoiceRouter from "@ibex/app/router/lightninginvoice";
+import lightningPaymentRouter from "@ibex/app/router/lightningpayment";
+import currencyRouter from "@ibex/app/router/currency";
+import onchainRouter from "@ibex/app/router/onchain";
 
-
-import lnurlRouter from '@ibex/app/router/lnurlpay';
+import lnurlRouter from "@ibex/app/router/lnurlpay";
 
 const router: Router = express.Router();
 
-router.use('/account', accountRoutes);
-router.use('/lightningAddress', lightningAddressRouter);
-router.use('/lightningInvoice', lightningInvoiceRouter);
-router.use('/lightningPayment', lightningPaymentRouter);
-router.use('/currency', currencyRouter);
-router.use('/onchain', onchainRouter);
+router.use("/account", accountRoutes);
+router.use("/lightningAddress", lightningAddressRouter);
+router.use("/lightningInvoice", lightningInvoiceRouter);
+router.use("/lightningPayment", lightningPaymentRouter);
+router.use("/currency", currencyRouter);
+router.use("/onchain", onchainRouter);
 
-
-
-router.use('/lnurl', lnurlRouter);
+router.use("/lnurl", lnurlRouter);
 
 export default router;
