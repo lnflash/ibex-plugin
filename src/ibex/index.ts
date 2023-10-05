fe2a15551eb87535ede9b991c1e6afc04f788c98
Dread <34528298+islandbitcoin@users.noreply.github.com>
2023-10-04 21:04:19 -0400

    change port to 8760

--- src/ibex/index.ts
+++ src/ibex/index.ts
@@ -1,10 +1,9 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const router = express.Router();
-import dotenv from 'dotenv';
-import routes from '@ibex/app/router'
-import { closeDatabase, setupDatabase } from '@ibex/knex';
-
+import dotenv from "dotenv";
+import routes from "@ibex/app/router";
+import { closeDatabase, setupDatabase } from "@ibex/knex";
 
 (async () => {
   try {
@@ -17,30 +16,26 @@ import { closeDatabase, setupDatabase } from '@ibex/knex';
     app.use(express.json());
 
     // Import and use the main router
-    app.use('/api/v1', routes);
+    app.use("/api/v1", routes);
 
     // Start the server
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 8760;
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
-      
-      
     });
   } catch (error) {
-    console.error('Error setting up the database:', error);
+    console.error("Error setting up the database:", error);
   }
 })();
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   try {
     await closeDatabase();
-    console.log('Database connection closed.');
+    console.log("Database connection closed.");
     process.exit(0);
   } catch (error) {
-    console.error('Error closing the database connection:', error);
+    console.error("Error closing the database connection:", error);
     process.exit(1);
   }
 });
 
 export default app;
-
-
